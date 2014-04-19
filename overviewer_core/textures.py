410d0ded005cd2734eecf3460f55100619a4a7ba
Andres Ruiz <andresruiz2010@gmail.com>
2014-04-19 17:50:13 -0400

    Add a small debug statement

--- overviewer_core/textures.py
+++ overviewer_core/textures.py
@@ -4278,6 +4278,7 @@ def flower(self, blockid, data):
 # Cobblestone
 @material(blockid=211, data=range(7), solid=True)
 def aligned_cobblestone_block(self, blockid, data):
+    print "Hit this"
     
     if data == 0:
         top = self.load_image_texture("assets/minecraft/textures/blocks/cobblestone/terrain-cobb-brickaligned.png")
